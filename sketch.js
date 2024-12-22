dfca6069bbad0c7b93a1824ccbe818d00f2b17d8
chaimass6 <chaitu.6914@gmail.com>
2024-12-22 11:25:52 +0530

    ok

--- sketch.js
+++ sketch.js
@@ -1,1 +1,11 @@
-const a = 0;
+let canvas
+        let current_tool = 'pencil'
+        let current_color = '#000000'
+        let current_thickness = 5
+
+        function setup() {
+            const canvas_width = min(710, windowWidth * 0.9)
+            canvas = createCanvas(canvas_width, 400)
+            canvas.parent('paper')
+            background(255)
+        }
